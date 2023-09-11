cc7c34f2635a26696cba4dfed19de1da947ab203
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-11 11:22:25 +0300

    added button  hover:cursor-pointer in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
             <div className='flex-1'>
                 <h1 className='text-4xl'>What ingredients do you have?</h1>
                 <p className='text-2xl pb-6'>They can surely be turned into delicious food, and anyone can cook!</p>
-                <button className='bg-yellow-800 text-white px-4 py-2 rounded-md'>Click here</button>
+                <button className='bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer'>Click here</button>
             </div>
             <img className='w-93' src='https://i.postimg.cc/qRLWYDYm/tofu-2.png' alt='tofu-image' />
         </div>

dd1bcf82176332b5b9da02cad213f7b5a1da10d6
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-11 11:57:17 +0300

    added bg-yellow-600 in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className='h-90 flex items-center pl-28'>
+        <div className='bg-yellow-600 h-90 flex items-center pl-28'>
             <div className='flex-1'>
                 <h1 className='text-4xl'>What ingredients do you have?</h1>
                 <p className='text-2xl pb-6'>They can surely be turned into delicious food, and anyone can cook!</p>

eb2303b526534f88d3ec31045d876a330b870d43
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-11 11:16:52 +0300

    added text-neutral-100 in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
     return (
-        <nav className='bg-yellow-800 h-14 flex justify-between items-center px-28 sticky top-0'>
+        <nav className='bg-yellow-800 text-neutral-100 h-14 flex justify-between items-center px-28 sticky top-0'>
             <h1>FOOD RECIPE</h1>
 
             <div className='flex gap-4 no-underline'>
-                <a className='no-underline' href='/'>Home</a>
-                <a className='no-underline' href='/recipe'>Recipe</a>
-                <a className='no-underline' href='/contact-us'>Contact us</a>
-                <a className='no-underline' href='/about-us'>About us</a>
+                <a className='text-neutral-100 no-underline' href='/'>Home</a>
+                <a className='text-neutral-100 no-underline' href='/recipe'>Recipe</a>
+                <a className='text-neutral-100 no-underline' href='/contact-us'>Contact us</a>
+                <a className='text-neutral-100 no-underline' href='/about-us'>About us</a>
             </div>
         </nav>
     )

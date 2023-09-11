15020d41ee09ff27c360d932b1aaaae8890cce40
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-11 11:18:09 +0300

    removed bg-yelow-500

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
-        <div className='h-90 bg-yellow-500 flex items-center pl-28'>
-            <div className='flex-1 line-hei'>
+        <div className='h-90 flex items-center pl-28'>
+            <div className='flex-1'>
                 <h1 className='text-4xl'>What ingredients do you have?</h1>
                 <p className='text-2xl pb-6'>They can surely be turned into delicious food, and anyone can cook!</p>
                 <button className='bg-yellow-800 text-white px-4 py-2 rounded-md'>Click here</button>

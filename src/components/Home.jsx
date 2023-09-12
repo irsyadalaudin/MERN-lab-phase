18c94640095c98b97dc285504ec2358e522dd84f
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-12 10:43:53 +0300

    added scrollDownToRecipe function in Home.jsx  and added id recipe in Recipe.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,18 @@
 import Recipe from './Recipe'
 
 const Home = () => {
+    const scrollDownToRecipe = () => {
+        const element = document.getElementById('recipe')
+            element.scrollIntoView({ behavior: 'smooth'})
+    }
+
     return (
         <>
             <div className='bg-yellow-600 h-90 flex items-center pl-28'>
                 <div className='flex-1'>
                     <h1 className='text-4xl'>What ingredients do you have?</h1>
                     <p className='text-xl pb-6'>They can surely be turned into delicious food, and anyone can cook!</p>
-                    <button className='bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer'>Click here</button>
+                    <button onClick={scrollDownToRecipe} className='bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer'>Click here</button>
                 </div>
                 <img className='w-93' src='https://i.postimg.cc/qRLWYDYm/tofu-2.png' alt='tofu-image' />
             </div>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,7 +4,7 @@
 const Recipe = () => {
     return (
         <>
-            <div className='bg-yellow-600 h-90 px-28'>
+            <div id='recipe' className='bg-yellow-600 h-90 px-28'>
                 <div>
                     <h1 className='text-4xl flex justify-center pt-10'>Let's cook !</h1>
                     <p className='text-xl leading-7'>"Have you ever been confused about what to eat? Want to buy food, but afraid of wasting money or having little money? Want to cook, but can't cook? Or can cook, but the ingredients are limited? Now we are here to provide a solution, especially for students who can still cook and eat deliciously, even with limited and cheap ingredients! Have you ever found yourself in a situation where you're not sure what to eat? Perhaps you're considering buying food, but you're worried about spending too much money or you have a tight budget. Maybe you have a desire to cook your own meals, but you lack the culinary skills. Or perhaps you're a skilled cook, but you find yourself with limited ingredients. We are here to offer a solution to these challenges, and our focus is particularly on students who want to enjoy delicious meals. Even with limited resources and budget-friendly ingredients, you can still prepare and savor tasty dishes!"</p>    

d7b976fe1b457b7b675b549597262d25ebfa4075
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-12 15:48:04 +0300

    added a href=/  in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
+// import { Link } from 'react-router-dom'
+
 /* eslint-disable no-mixed-spaces-and-tabs */
 const Navbar = () => {
     return (
         <nav className='bg-yellow-800 text-neutral-100 h-14 flex justify-between items-center px-28 sticky top-0 hover:cursor-pointer'>
-            <h1>FOOD RECIPE</h1>
+            <h1>
+                <a className='text-neutral-100 no-underline' href='/'>FOOD RECIPE</a>
+            </h1>
 
             <div className='flex gap-4 no-underline'>
                 <a className='text-neutral-100 no-underline' href='/'>Home</a>

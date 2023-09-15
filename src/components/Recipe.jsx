1155e01a462aece24123231cc8d8a58506f3c45c
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-15 07:44:30 +0300

    changed h1 text-4xl flex justify-center pt-8,  changed p text-2xl,  changed form flex justify-center px-28,  changed textarea resize-none text-xl w-97 h-56,   removed button inside form,  and added buttom outside form: button  float-right self-end h-8 w-20 mt-2 mx-28

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,13 +4,13 @@ const Recipe = () => {
         <>
             <div id='recipe' className='bg-yellow-600 h-90 px-28'>
                 <div>
-                    <h1 className='text-4xl flex justify-center pt-10'>Let's cook !</h1>
-                    <p className='text-xl leading-7'>"Have you ever been confused about what to eat? Want to buy food, but afraid of wasting money or having little money? Want to cook, but can't cook? Or can cook, but the ingredients are limited? Now we are here to provide a solution, especially for students who can still cook and eat deliciously, even with limited and cheap ingredients! Have you ever found yourself in a situation where you're not sure what to eat? Perhaps you're considering buying food, but you're worried about spending too much money or you have a tight budget. Maybe you have a desire to cook your own meals, but you lack the culinary skills. Or perhaps you're a skilled cook, but you find yourself with limited ingredients. We are here to offer a solution to these challenges, and our focus is particularly on students who want to enjoy delicious meals. Even with limited resources and budget-friendly ingredients, you can still prepare and savor tasty dishes!"</p>    
+                    <h1 className='text-4xl flex justify-center pt-8'>Let's cook !</h1>
+                    <p className='text-2xl'>"Have you ever been confused about what to eat? Want to buy food, but afraid of wasting money or having little money? Want to cook, but can't cook? Or can cook, but the ingredients are limited? Now we are here to provide a solution, especially for students who can still cook and eat deliciously, even with limited and cheap ingredients! Have you ever found yourself in a situation where you're not sure what to eat? Perhaps you're considering buying food, but you're worried about spending too much money or you have a tight budget. Maybe you have a desire to cook your own meals, but you lack the culinary skills. Or perhaps you're a skilled cook, but you find yourself with limited ingredients. We are here to offer a solution to these challenges, and our focus is particularly on students who want to enjoy delicious meals. Even with limited resources and budget-friendly ingredients, you can still prepare and savor tasty dishes!"</p>    
                 </div>
-                <form className='flex justify-center self-start relative'>
-                    <textarea className='resize-none text-xl w-97 h-64' placeholder='Enter your ingredients'></textarea>
-                    <button className='float-right mt-2 mb-3 mr-26 absolute bottom-0 right-0 h-8 w-16'>Search</button>
+                <form className='flex justify-center px-28'>
+                    <textarea className='resize-none text-xl w-97 h-56' placeholder='Enter your ingredients'></textarea>
                 </form>
+                    <button className='float-right self-end h-8 w-20 mt-2 mx-28'>Search</button>
             </div>
         </>
     )

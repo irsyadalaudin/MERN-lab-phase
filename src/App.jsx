f1fef26ad9c97c906a8421535401d73ff9a5b008
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-15 07:46:23 +0300

    imported ContactUs.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Recipe from './components/Recipe'
+import ContactUs from './components/ContactUs'
 
 const App = () => {
 	return (
@@ -11,6 +12,7 @@ const App = () => {
 		<Routes>
 			<Route path = '/' element={<Home />} />
 			<Route path = '/recipe' element={<Recipe />} />
+			<Route path = '/contact-us' element={<ContactUs />} />
 		</Routes>
 		</>
 	)

4e11f70f6b40a4f2f5dc8ab463b72c52ee3be198
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-15 20:18:03 +0300

    changed a into Link and added scrollOnTop function in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-// import { Link } from 'react-router-dom'
-
 /* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-undef */
+
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
+        const scrollToTop = () => {
+            windows.scrollTo({ top: 0, behavior: 'smooth' })
+        }
+
     return (
         <nav className='bg-yellow-800 text-neutral-100 h-14 flex justify-between items-center px-28 sticky top-0 hover:cursor-pointer'>
             <h1>
-                <a className='text-neutral-100 no-underline' href='/'>AYO MASAK</a>
+                {/* <a className='text-neutral-100 no-underline' href='/'>AYO MASAK</a> */}
+                <Link to='/' onClick={scrollToTop}>AYO MASAK</Link>
             </h1>
             <div className='flex gap-4 no-underline'>
                 <a className='text-neutral-100 no-underline' href='/'>Home</a>

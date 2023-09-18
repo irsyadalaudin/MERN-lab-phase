7532f5543c7af5681b2e346d1448fa7210c867dc
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-18 08:15:48 +0300

    removed space tab in path   at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ const App = () => {
 		<>
 			<Navbar />
 			<Routes>
-				<Route path= '/' element={<Home />} />
-				<Route path= '/' element={<Recipe />} />
-				<Route path= '/contact-us' element={<ContactUs />} />
-				<Route path= '/about-us' element={<AboutUs />} />
-				<Route path= '/login' element={<Login />} />
+				<Route path='/' element={<Home />} />
+				<Route path='/' element={<Recipe />} />
+				<Route path='/contact-us' element={<ContactUs />} />
+				<Route path='/about-us' element={<AboutUs />} />
+				<Route path='/login' element={<Login />} />
 			</Routes>
 		</>
 	)

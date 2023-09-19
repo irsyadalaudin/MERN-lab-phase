d383b8fdc04147568b8471b931d4db9fd9bbd78b
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-19 08:49:21 +0300

    changed form into mt-2 and  changed textarea h-32 in ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,17 +3,17 @@ const ContactUs = () => {
     return (
         <div className='bg-yellow-600 h-90 px-28'>
             <h1 className='text-4xl flex justify-center pt-8'>Contact Us</h1>
-            <div className='text-2xl font-bold'>
+            <div className='text-2xl font-bold text-justify'>
                 <p className='mt-4'>If you ever find yourself in a culinary conundrum, unsure of what to enjoy for your meal, or hesitant to spend too much money on food, fear not! Our "Contact Us" page is here to help. Whether you're a cooking enthusiast looking to expand your skills or a skilled chef working with limited ingredients, we're ready to assist you.</p>
                 <p className='mt-4'>Feel free to reach out to us with any questions, suggestions, or inquiries. We're dedicated to making your culinary journey enjoyable and budget-friendly. Together, let's explore the world of delightful dishes that anyone can cook, regardless of their culinary expertise or budget limitations.</p>
                 <p className='mt-4'>Contact us today, and let's embark on a flavorful adventure!</p>
             </div>
-            <form className='flex flex-col self-center mt-4 px-28'>
+            <form className='flex flex-col self-center mt-2 px-28'>
                 <input className='placeholder-white focus:outline-none w-64 text-lg mb-2 bg-yellow-800 text-white rounded' type='text' placeholder='name'/>
                 <input className='placeholder-white focus:outline-none w-64 text-lg mb-0 bg-yellow-800 text-white rounded' type='email' placeholder='email' />
             </form>
             <form className='flex justify-center px-28 mt-2'>
-                <textarea className='placeholder-white focus:outline-none resize-none text-xl p-3 w-97 h-36 bg-yellow-800 text-white rounded-md' placeholder='Enter your message'></textarea>
+                <textarea className='placeholder-white focus:outline-none resize-none text-xl p-3 w-97 h-32 bg-yellow-800 text-white rounded-md' placeholder='Enter your message'></textarea>
             </form>
             <button className='float-right self-end h-8 w-20 mt-2 mx-28 bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>Search</button>
         </div>

215a9ca23bf37f37d637c45f20b1e842567d45cd
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-23 17:18:50 +0300

    removed unecessary comment in Account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,4 +1,3 @@
-// import { useEffect, useRef, useState } from 'react'
 import { useEffect, useState } from 'react'
 
 const Account = () => {
@@ -9,7 +8,6 @@ const Account = () => {
     const [name, setName] = useState('Joko')
     const [userName, setUserName] = useState('joko0123')
     const [email, setEmail] = useState('jokomiyaw@gmail.com')
-    // const contentRef = useRef(null)
 
     const moveTab = (tabName) => {
         return () => {
@@ -58,19 +56,6 @@ const Account = () => {
         }
     }
 
-    // useEffect(() => {
-    //     // Update the class of the container based on content height
-    //     if (contentRef.current) {
-    //         const contentHeight = contentRef.current.offsetHeight;
-    //         const container = document.getElementById('container');
-    //         if (contentHeight > container.clientHeight) {
-    //             container.classList.add('fullHeight');     // Apply the class for full height
-    //         } else {
-    //             container.classList.remove('fullHeight');  // Remove the class if not needed
-    //         }
-    //     }
-    // });
-
     useEffect(() => {
         const container = document.getElementById('container')
         const contentHeight = container.scrollHeight
@@ -86,13 +71,11 @@ const Account = () => {
     const containerStyle = `bg-yellow-600 px-28 ${contentExceddHeight ? 'h-full' : 'h-90'}`
 
     return (
-        // <div id='container' className='bg-yellow-600 h-90 px-28'>
         <div id='container' className={containerStyle}>
             <div className='text-center pt-8'>
                 <h1 className='pb-4 text-4xl'>Account</h1>
                 <p className='text-2xl'>Manage your personal information, including phone numbers and email address where you can be contacted</p>
             </div>
-            {/* <div className='flex flex-grow gap-14' ref={contentRef}> */}
             <div className='flex flex-grow gap-14'>
                 <div className='w-1/3 mt-5'>
                     <div className='flex flex-col'>
@@ -102,7 +85,6 @@ const Account = () => {
                         <button onClick={moveTab('account-setting')} className='bg-transparent h-24'>Account Setting</button>
                     </div>
                 </div>
-                {/* <div className='w-2/3 mt-5' ref={contentRef}> */}
                 <div className='w-2/3 mt-5'>
                     {selectedTab === 'personal-information' && (
                         <form action='#'>

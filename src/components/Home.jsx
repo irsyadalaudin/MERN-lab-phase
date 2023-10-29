05654ed8bd3777b679218064fabcfab37847570b
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-10-29 13:22:35 +0200

    removed unnecessary comment in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,6 @@ const Home = () => {
                     <button onClick={scrollDownToRecipe} className='bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>Click here</button>
                 </div>
                 <img className='xl:w-90 2xl:w-93 sm:h-full xl:h-90' src='https://i.postimg.cc/qRLWYDYm/tofu-2.png' alt='tofu-image' />
-                {/* absolute top-0 left-0 z-0 opacity-50 md:z-10 md:opacity-100 */}
             </div>
             <Recipe />
         </>

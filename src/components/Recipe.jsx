479f5524343fd4f7515de431852730201e07d976
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-10-29 16:13:46 +0200

    repaired padding bottom for  button search  in Recipe.jsx

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -114,7 +114,7 @@ const Recipe = () => {
         }
     }, [recipes]) 
 
-    const containerStyle = `bg-yellow-600 px-10 lg:px-28 h-full ${contentExceedHeight ? 'h-full' : 'lg:h-90'}`
+    const containerStyle = `bg-yellow-600 px-10 lg:px-28 h-full ${contentExceedHeight ? 'h-full' : 'xl:h-90'}`
 
 
     // USE EFFECT TO UPDATE MINIMIZE RESPONSIVE WINDOW WIDTH BACKGROUND
@@ -168,7 +168,7 @@ const Recipe = () => {
                 <div className='flex justify-center'>
                     <input onChange={handleIngredientsInput} onKeyDown={disabledEnter} ref={inputRef} value={inputValue} className='placeholder-white focus:outline-none text-xl p-3 w-30 md:w-full xl:w-94 h-20 bg-yellow-800 text-white rounded-md block' placeholder='Enter your ingredients' />
                 </div>
-                <div className='flex justify-center md:justify-end'>
+                <div className='flex justify-center md:justify-end pb-4 xl:pb-0'>
                     <button onClick={() => {searchRecipeButton(); setIsRecipeVisible(true); setIsRecipeDetailVisible(false); setContentExceedHeight(true)}} disabled={disabledSubmit()} type='submit' className='float-right self-end h-8 w-20 mt-2 mx-0 xl:mx-45 bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>Search</button>  {/* IF input empty OR isRecipeAvailable IS TRUE, then SEARCH BUTTON WILL BE DISABLED */}
                 </div>
             </form>

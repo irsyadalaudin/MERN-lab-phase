243ab4ecee4c47984779cacf64d003550af11c59
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-03 22:36:58 +0200

    repaired configureStore({rootReducer}) into configureStore({reducer: rootReducer} in store.js

--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducer/Reducer';
 
-const store = configureStore(rootReducer);
+// const store = configureStore(rootReducer);
+const store = configureStore({
+    reducer: rootReducer
+});
 
 export default store;

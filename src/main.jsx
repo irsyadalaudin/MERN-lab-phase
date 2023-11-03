75b5d92788bc9d4d0f0c707f31b3a4064a97eb7b
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-03 21:29:30 +0200

    imported Provider from react-redux in  and reorder  inmport BrowserRouter  in  main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './components/store/store.js'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</BrowserRouter>,
 )

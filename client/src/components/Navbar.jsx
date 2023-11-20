210db39f1ab45f97e691bdb0ca552f0eb5130015
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-20 16:22:01 +0200

    added ayo masak logo   in Navabr.jsx and Login.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,7 +26,9 @@ const Navbar = () => {
     return (
         <nav className='bg-yellow-800 text-neutral-100 md:h-20 lg:h-14 py-3 lg:py-0 px-0 md:px-28 flex-col lg:flex-row flex justify-between items-center sticky top-0 hover:cursor-pointer'>
             <h1>
-                <Link to='/' onClick={scrollToTopHome}>AYO MASAK</Link>
+                {/* <Link to='/' onClick={scrollToTopHome}>AYO MASAK</Link> */}
+                <Link to='/' onClick={scrollToTopHome}><img className='mt-0 xl:mt-2' src='https://i.postimg.cc/BZFTkfL1/ayo-masak-logo.png' alt='' /></Link>
+
             </h1>
             <div className='mt-5 md:mt-0 flex gap-4 no-underline text-sm sm:text-base'>
                 <Link to='/' onClick={scrollToTopHome} className='text-neutral-100 no-underline'>Home</Link>

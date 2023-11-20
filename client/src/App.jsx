98e669c4ac8938f76d43c9f4f8f1472875206e7a
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-20 15:47:54 +0200

    1) added useNavigate and useLocation   2) if isLogged in falsy useNavigate into /login,   3) and hide <Navbar /> IF THE CURRENT PAGE IS /login     in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
@@ -6,14 +6,32 @@ import Recipe from './components/Recipe'
 import ContactUs from './components/ContactUs'
 import AboutUs from './components/AboutUs'
 import Account from './components/Account'
+import Login from './components/Login'
+import { useEffect } from 'react'
 
 const App = () => {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		const isLoggedIn = localStorage.getItem('isLoggedIn');
+		if (!isLoggedIn) {
+			navigate('/login');
+		}
+	}, [navigate]);
+
+	// CHECK IF THE CURRENT PAGE IS '/login'
+	const hideNavbar = location.pathname === '/login';
+
+
 	return (
 		<>
-			<Navbar />
+			{hideNavbar ? null : <Navbar />}
+			{/* <Navbar /> */}
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/' element={<Recipe />} />
+				<Route path='/login' element={<Login />} />
 				<Route path='/contact-us' element={<ContactUs />} />
 				<Route path='/about-us' element={<AboutUs />} />
 				<Route path='/account' element={<Account />} />

1b7bb7a1548e5b017b3e1926b375a2837caf11b5
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-21 17:51:57 +0200

    added module.exports.signup_post = async (req, res) => { }   in loginControllers.js

--- server/controllers/loginControllers.js
+++ server/controllers/loginControllers.js
@@ -1,3 +1,5 @@
+import User from '../models/User';
+
 module.exports.signup_get = (req, res) => {
     res.render('signUp');
 }
@@ -6,10 +8,21 @@ module.exports.login_get = (req, res) => {
     res.render('signIn');
 }
 
-module.exports.signup_post = (req, res) => {
-    res.render('new-signUp');
+// ASYHNCRONOUS FUNCTION
+module.exports.signup_post = async (req, res) => {
+    const {email, password} = req.body;
+    console.log(email, password);
+    res.send('user login');
+    
+    try {
+        const user = await User.create({email, password});
+        res.status(201).json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({err: 'user not created'});
+    }
 }
 
-module.exports.signup_get = (req, res) => {
+module.exports.login_post = (req, res) => {
     res.render('user-signIn');
 }

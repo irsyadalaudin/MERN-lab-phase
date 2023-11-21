f10463b8dcead479620b41b830a6f97776a06ddf
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-21 14:48:31 +0200

    added Register.jsx routes in   App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,16 +7,17 @@ import ContactUs from './components/ContactUs'
 import AboutUs from './components/AboutUs'
 import Account from './components/Account'
 import Login from './components/Login'
+import Register from './components/Register'
 import { useEffect } from 'react'
 
 const App = () => {
-	const navigate = useNavigate();
-	const location = useLocation();
+	const navigate = useNavigate()
+	const location = useLocation()
 
 	useEffect(() => {
-		const isLoggedIn = localStorage.getItem('isLoggedIn');
+		const isLoggedIn = localStorage.getItem('isLoggedIn')
 		if (!isLoggedIn) {
-			navigate('/login');
+			navigate('/login')
 		}
 	}, [navigate]);
 
@@ -26,12 +27,14 @@ const App = () => {
 
 	return (
 		<>
+			{/* DI COMMENT DULU YANG DIBAWAH NI BIAR BISA NGEDIT YANG LAINNYA */}
 			{hideNavbar ? null : <Navbar />}
 			{/* <Navbar /> */}
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/' element={<Recipe />} />
 				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
 				<Route path='/contact-us' element={<ContactUs />} />
 				<Route path='/about-us' element={<AboutUs />} />
 				<Route path='/account' element={<Account />} />

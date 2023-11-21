6165ae32a871d6e3b8779d77f0ca9174b17bc524
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-21 21:08:47 +0200

    repaired /register and /login routes   in routes/loginRoutes.js

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -3,9 +3,9 @@ import * as loginControllers from '../controllers/loginControllers.js';
 
 const router = Router()
 
-router.get('/signUp', loginControllers.signup_get);
-router.post('/signUp', loginControllers.signup_post);
-router.get('/signIn', loginControllers.login_get);
-router.post('/signIn', loginControllers.login_post);
+router.get('/register', loginControllers.register_get);
+router.post('/register', loginControllers.register_post);
+router.get('/login', loginControllers.login_get);
+router.post('/login', loginControllers.login_post);
 
 export default router;

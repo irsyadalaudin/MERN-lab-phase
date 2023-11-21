80e3194db475410350765a17b7b147accab9b4ed
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-21 17:50:10 +0200

    changed import loginController into loginControllers   in loginRotes.jsx

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -1,9 +1,9 @@
 import {Router} from 'express';
-import loginController from '../controllers/loginController.js';
+import loginControllers from '../controllers/loginControllers.js';
 
 const router = Router()
 
-router.get('/signUp', loginController.signup_get);
-router.post('/signUp', loginController.signup_post);
-router.get('/signIn', loginController.login_get);
-router.post('/signIn', loginController.login_post);
+router.get('/signUp', loginControllers.signup_get);
+router.post('/signUp', loginControllers.signup_post);
+router.get('/signIn', loginControllers.login_get);
+router.post('/signIn', loginControllers.login_post);

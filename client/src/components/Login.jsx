75953b053f26b27757eca5ef21705eb3a9ba645a
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-21 14:46:07 +0200

    1) removed unncessary function   2) split sign in and sign up, sign in in Login.jsx and sign up in Register.jsx   3) added Link to /register   in Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,48 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-    const [showSignIn, setShowSignIn] = useState(true)
-    const [disableSignUp, setDisableSignUp] = useState(false)
-
-    const handleToggleSignIn = () => {
-        setShowSignIn(true)
-        setDisableSignUp(false)
-        document.getElementById('sign-in-btn').disable = true 
-    }
-
-    const handleToggleSignUp = () => {
-        setShowSignIn(false)
-        setDisableSignUp(true)
-        document.getElementById('sign-up-btn').disable = true    
-    }
-
     return (
-        <div className='bg-yellow-600 h-89 flex items-center pl-10 overflow-hidden'>
+        <div className='bg-yellow-600 h-90 flex items-center pl-10 overflow-hidden'>
             <div className='w-2/3 xl:w-1/2 text-center'>
                 <img src='https://i.postimg.cc/jjCNhpY2/ayo-masak-logo.png' alt='ayo-masak-logo' />
                 <div className='flex justify-center'>
                     <p className='mb-6 text-2xl text-justify flex justify-center w-85'>Embark on a delightful culinary journey with us at AYO MASAK! &ensp; Let's make every meal an enjoyable and budget-friendly experience</p>
                 </div>
-                <button onClick={handleToggleSignIn} disabled={showSignIn ? true : false} id='sign-in-btn' className='p-1 mb-2 bg-amber-700 rounded-l-lg hover: cursor-pointer hover:bg-amber-800'>Sign In</button>
-                <button onClick={handleToggleSignUp} disabled={disableSignUp} id='sign-up-btn' className='p-1 mb-2 bg-amber-700 rounded-r-lg hover: cursor-pointer hover:bg-amber-800'>Sign Up</button>
+                {/* BUTTON Link to REGISTER */}
+                <button disabled className='p-1 mb-2 bg-amber-700 rounded-l-lg hover: cursor-pointer hover:bg-amber-800' >Sign In</button>
+                <button id='sign-up-btn' className='p-1 mb-2 bg-amber-700 rounded-r-lg hover: cursor-pointer hover:bg-amber-800'>
+                    <Link to='/register' className='text-white no-underline' id='sign-in-btn'>Sign Up</Link>
+                </button>
                 {/* SIGN IN */}
-                <form className='flex flex-col items-center px-0 lg:px-20 xl:px-28' style={{ display: showSignIn ? 'block' : 'none' }}>
+                <form className='flex flex-col items-center px-0 lg:px-20 xl:px-28'>
                     <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='text' placeholder='Email / Username' />
                     <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='password' placeholder='Password' />
                     <div className='flex justify-center xl:justify-end'>
-                        <p className='mt-2 mr-4 ml-0 xl:ml-9'>Don't have an account?<a onClick={handleToggleSignUp} href='#' className='ml-1 no-underline'>sign up</a></p>
-                        <button type='submit' className='h-8 w-20 mt-2 mx-0 xl:mx-10 bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>Login</button>  {/* IF input empty OR isRecipeAvailable IS TRUE, then SEARCH BUTTON WILL BE DISABLED */}
-                    </div>
-                </form>
-                {/* SIGN UP */}
-                <form className='flex flex-col items-center px-0 xl:px-28' style={{ display: showSignIn ? 'none' : 'block' }}>
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='text' placeholder='Name' />
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='text' placeholder='Username' />
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='email' placeholder='Email' />
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='password' placeholder='Password' />
-                    <div className='flex justify-center xl:justify-end'>
-                        <button type='submit' className='float-right self-end h-8 w-20 mt-2 mx-0 xl:mx-9 bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>Register</button>  {/* IF input empty OR isRecipeAvailable IS TRUE, then SEARCH BUTTON WILL BE DISABLED */}
+                        <p className='mt-3 mr-4 ml-0 xl:ml-9'>
+                            <a className='no-underline' href=''>Don't have an account? </a>
+                        </p>
+                        <button type='submit' className='h-8 w-20 mt-2 mx-0 xl:mx-10 bg-yellow-800 text-white px-4 py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>Login</button>
                     </div>
                 </form>
             </div>

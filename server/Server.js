c56fbd23276ec775343d6581039257e623f9a773
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-25 04:26:44 +0200

    1) added /user, userRoutes   2) reorder mongodb connection   in Server.js

--- server/Server.js
+++ server/Server.js
@@ -5,12 +5,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { routes } from './routes/recipeRoutes.js';
 import cors from 'cors';
-import loginRoutes from './routes/loginRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+// ROUTES
+app.use('/', routes);
+app.use('/user', userRoutes);
+
 // MONGODB DATABASE CONNECTION
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
@@ -23,12 +30,6 @@ mongoose.connect(process.env.MONGO_URI, {
 		console.log('error connecting to MongoDB', err);
 	})
 
-app.use(express.json());
-app.use(cors());
-
-// ROUTES
-app.use('/', routes);
-app.use(loginRoutes);
 
 app.listen(port, () => {
 	console.log(`server running at port http://localhost:${port}`);

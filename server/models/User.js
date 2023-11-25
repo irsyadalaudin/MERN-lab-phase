65a934c658560628cdeccf0aab6b6619723cf102
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-25 16:39:31 +0200

    added userSchema for login   in models/User.js

--- server/models/User.js
+++ server/models/User.js
@@ -53,7 +53,6 @@ userSchema.statics.register = async function(name, username, email, password) {
     })
 
     const exists = await this.findOne({ email })
-
     if (exists) {
         throw Error('Email already in use !')
     }
@@ -65,6 +64,27 @@ userSchema.statics.register = async function(name, username, email, password) {
     const user = await this.create({ name, username, email, password: hash })
     return user
 }
+
+
+// STATIC LOGIN (SIGN IN) METHOD
+userSchema.statics.login = async function(email, password) {
+    // VALIDATION
+    if (!email || !password) {
+        throw Error('All fields must be filled!')
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error('Incorrect Email!')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+        throw Error('Incorrect Password!')
+    }
+    return user
+}
+
 
 const User = mongoose.model('User', userSchema)
 export default User

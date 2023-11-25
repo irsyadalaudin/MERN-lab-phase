12bd2691700eb35df2a5e516b2b0a6be161fd763
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-25 17:04:15 +0200

    imported AuthContextProvider  and added it as a Provider parent and BrowserRouter child tag   in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './components/store/store.js'
-import App from './App.jsx'
-import './index.css'
+import { AuthContext, AuthContextProvider } from './context/AuthContext.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<AuthContextProvider>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</AuthContextProvider>
 	</BrowserRouter>,
 )

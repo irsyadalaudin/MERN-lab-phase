89219911c040eabad98af2e78bfb88968f1b9fa1
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-25 13:37:42 +0200

    added createToken function   in controllers/userControllers.js

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,11 @@
 import User from '../models/User.js'
 import jwt from 'jsonwebtoken'
 
+// JWT
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '1d'})
+}
+
 // LOGIN USER
 export const loginUser = async (req, res) => {
     res.json({message: 'login user'})
@@ -12,7 +17,12 @@ export const registerUser = async (req, res) => {
 
     try {
         const user = await User.register(name, username, email, password)
-        res.status(200).json({ name, username, email, user })              // INI HARUS DITULIS SEMUANYA 
+
+        // CREATE A TOKEN
+        const token = createToken(user._id)
+
+        // res.status(200).json({ name, username, email, user })              // INI HARUS DITULIS SEMUANYA 
+        res.status(200).json({ name, username, email, token })
     } catch (err) {
         // res.status(400).json({ message: 'error', err })
         res.status(400).json({ message: err.message })

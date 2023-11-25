e9feb0e09b60f58b4c42e6d7420246bf3b0e3ea9
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-25 04:37:48 +0200

    added comment `how to use router in postman`   in routes/userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,3 +9,10 @@ router.post('/login', loginUser)
 router.post('/register', registerUser)
 
 export default router
+
+/* CARA MENGGUNAKAN ROUTER DI POSTMAN:
+1) nyalain server di terminal:   npm run start
+2) masuk ke postman, pilih method post, pilih endpoint users/login
+3) pastikan memilih body, pilih raw, pilih json, pilih tambahkan data yang ingin dikirimkan
+4) masukkan username dan password yang sesuai
+*/

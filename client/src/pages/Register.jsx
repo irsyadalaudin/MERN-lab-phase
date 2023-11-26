5a35027058c975fca073130e91cb95ded29229ed
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-26 10:33:24 +0200

    1) added useState for name, username, email, password   2) added handleSubmit function  in client/src/pages/Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useRegister } from '../hooks/useRegister.js'
 
 const Register = () => {
     const [name, setName] = useState('')
-    const [username, sertUsername] = useState('')
+    const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { register, error, isLoading } = useRegister()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(loginInput, password)
+        // console.log(name, username, email, password)
+        await register(name, username, email, password)
+        setName('')
+        setUsername('')
+        setEmail('')
+        setPassword('')
     }
 
     return (
@@ -26,13 +33,14 @@ const Register = () => {
                 <button disabled className='p-1 mb-2 bg-amber-700 rounded-r-lg hover: cursor-pointer hover:bg-amber-800'>SignUp</button>
                 {/* SIGN UP */}
                 <form onSubmit={handleSubmit} className='flex flex-col items-center px-0 xl:px-28' >
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='text' placeholder='Name' />
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='text' placeholder='Username' />
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='email' placeholder='Email' />
-                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='password' placeholder='Password' />
+                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='text' onChange={(e) => setName(e.target.value)}  value={name} placeholder='Name' autoComplete='Name' />
+                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='text' onChange={(e) => setUsername(e.target.value)}  value={username} placeholder='Username' autoComplete='Username' />
+                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='email' onChange={(e) => setEmail(e.target.value)} value={email} placeholder='Email' autoComplete='Email' />
+                    <input className='placeholder-white focus:outline-none w-80 text-lg p-2 mb-2 bg-yellow-800 text-white rounded-lg' type='password' onChange={(e) => setPassword(e.target.value)}  value={password} placeholder='Password' autoComplete='Password' />
                     <div className='flex justify-center xl:justify-end'>
-                        <button type='submit' className='h-8 w-20 mt-2 mx-0 xl:mx-9 bg-yellow-800 text-white py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>Register</button>
+                        <button disabled={isLoading} type='submit' className='h-8 w-20 mt-2 mx-0 xl:mx-9 bg-yellow-800 text-white py-2 rounded-md hover:cursor-pointer hover:bg-yellow-900'>SIgn Up</button>
                     </div>
+                    {error && <div className='error'>{error}</div>}
                 </form>
             </div>
             {/* IMAGE */}

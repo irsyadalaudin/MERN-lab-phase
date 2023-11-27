22acfdc197a862012727051e7913c47f0259993d
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-27 10:45:24 +0200

    1) moved handleLogout & useEffect() from Navbar.jsx into App.jsx   2) sent props from App.jsx into Navbar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom'
 
 import './App.css'
@@ -9,12 +10,46 @@ import AboutUs from './components/AboutUs'
 import Account from './components/Account'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import { useEffect, useState } from 'react'
+import { useLogout } from './hooks/useLogout' 
+import { useAuthContext } from './hooks/useAuthContext'
 
 const App = () => {
+    const [showNavbar, setShowNavbar] = useState(false)
+
+    const { Logout } = useLogout()
+    const { user } = useAuthContext()
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        Logout()
+        navigate('/login')
+    }
+
+    useEffect(() => {
+        setShowNavbar(!!user && window.location.pathname !== '/login')
+    }, [user])
 
     return (
-        <>
-            <Navbar />
+        <>  
+            {showNavbar && (
+                <>
+                    {/* {user && <Navbar />} */}
+                    {user && <Navbar user={user} onLogout={handleLogout} />}
+                    {/* {user && (
+                        <div className='flex justify-center bg-yellow-600 rounded-md gap-2 pl-1'>
+                            <span className='text-black'>{user.username}</span>
+                            <button onClick={handleLogout} className='bg-yellow-600 text-black rounded-md hover:cursor-pointer hover:bg-yellow-700 border-none'>Logout</button>
+                        </div>
+                    )} */}
+                    {showNavbar && !user && (
+                        <div>
+                            <Link to='/login' className='text-neutral-100 no-underline'>Login</Link>
+                        </div>
+                    )}
+                </>
+            )}
+            {/* <Navbar /> */}
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/' element={<Recipe />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,11 @@
-import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
-import { useAuthContext } from '../hooks/useAuthContext'
 
-const Navbar = () => {
-    const [navbarVisibility, setNavbarVisibility] = useState(false)
-
-    const { Logout } = useLogout()
-    const { user } = useAuthContext()
+const Navbar = ({ user, onLogout }) => {
+    const navigate = useNavigate()
 
     const scrollToTopHome = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' })
     }
-
-    const navigate = useNavigate()
     const scrollToBottomRecipe = (e) => {
         e.preventDefault()
         if (location.pathname === '/') {
@@ -28,19 +20,10 @@ const Navbar = () => {
         }
     }
 
-    const handleLogout = () => {
-        Logout()
-        navigate('/login')
-    }
-
-    useEffect(() => {
-        // Update visibility based on user status
-        setNavbarVisibility(!!user)
-    }, [user])
 
     return (
         <>
-            {navbarVisibility && (
+            {/* {navbarVisibility && ( */}
                 <nav className='bg-yellow-800 text-neutral-100 md:h-20 lg:h-14 py-3 lg:py-0 px-0 md:px-28 flex-col lg:flex-row flex justify-between items-center sticky top-0 hover:cursor-pointer'>
                     <h1>
                         <Link to='/' onClick={scrollToTopHome}>
@@ -63,20 +46,21 @@ const Navbar = () => {
                         <Link to='/account' className='text-neutral-100 no-underline'>
                             Account
                         </Link>
+
                         {user && (
                             <div className='flex justify-center bg-yellow-600 rounded-md gap-2 pl-1'>
                                 <span className='text-black'>{user.username}</span>
-                                <button onClick={handleLogout} className='bg-yellow-600 text-black rounded-md hover:cursor-pointer hover:bg-yellow-700 border-none'>Logout</button>
+                                <button onClick={onLogout} className='bg-yellow-600 text-black rounded-md hover:cursor-pointer hover:bg-yellow-700 border-none'>Logout</button>
                             </div>
                         )}
-                        {!user && (
+                        {/* {!user && (
                             <div>
                                 <Link to='/login' className='text-neutral-100 no-underline'>Login</Link>
                             </div>
-                        )}
+                        )} */}
                     </div>
                 </nav>
-            )}
+            {/* )} */}
         </>
     )
 }

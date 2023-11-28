a95054c07df7fe21e219ebf8dee414bd6366955b
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-28 07:00:40 +0200

    reordered import   in Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
-import { useNavigate } from 'react-router-dom'
 /* eslint-disable react/no-unescaped-entities */
+import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useRegister } from '../hooks/useRegister.js'

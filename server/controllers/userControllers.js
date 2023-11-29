ded5fb57c8b59cad9559add70f7a52e402d82f28
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-29 10:30:17 +0200

    repaired comment   in server/controllers/userControllers.js

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -13,13 +13,14 @@ export const loginUser = async (req, res) => {
     try {
         const user = await User.login(identifier, password)
 
-        // CREATE A TOKEN
+        // CREATE A JWT TOKEN
         const token = createToken(user._id)
+
+        // THESE ARE ALL REQUIRED { identifier, email, token }, AND MUST BE WRITTEN DOWN
         res.status(200).json({ username: user.username, email: user.email, token })
     } catch (err) {
         res.status(400).json({ message: err.message})
     }
-    // res.json({message: 'login user'})
 }
 
 // REGISTER USER  // NYAMBUNG SAMA models/User.js
@@ -29,12 +30,12 @@ export const registerUser = async (req, res) => {
     try {
         const user = await User.register(name, username, email, password)
 
-        // CREATE A TOKEN
+        // CREATE A JWT TOKEN
         const token = createToken(user._id)
-        // res.status(200).json({ name, username, email, user })              // INI HARUS DITULIS SEMUANYA 
+
+        // THESE ARE ALL REQUIRED { name, username, email, token }, AND MUST BE WRITTEN DOWN
         res.status(200).json({ name, username, email, token })
     } catch (err) {
-        // res.status(400).json({ message: 'error', err })
         res.status(400).json({ message: err.message })
     }
 }

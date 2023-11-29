02abcdcfe29f7fe7c72b5c8e1af37c092b714c64
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-11-29 10:36:30 +0200

    1)repaired router.get(`/login, )  2)repaired comment   `in server/routes/userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,58 +1,33 @@
 // server/routes/userRoutes.js
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/userControllers.js';
-import { userAuth } from '../middlewares/userAuth.js';
-import User from '../models/User.js';
+import { loginUser, registerUser } from '../controllers/userControllers.js'
+import { userAuth } from '../middlewares/userAuth.js'
+import User from '../models/User.js'
 
 const router = express.Router();
 
-<<<<<<< HEAD
-// Rute yang memerlukan otentikasi pengguna
-router.get('/profile', userAuth, async (req, res) => {
+// GET LOGIN AND REGISTER ROUTE
+// ROUTES THAT REQUIRE USER AUTHENTICATION
+router.get('/login', userAuth, async (req, res) => {
     try {
-        const user = await User.findById(req.user._id);
-        res.status(200).json({
+        const user = await User.findById(req.user._id)
+        const signedUsers = user.map(user => ({
             username: user.username,
             email: user.email,
             name: user.name,
-        });
-=======
-router.get('/login', async (req, res) => {
-    // const { query } = req.query
-
-    try {
-        const users = await User.find({ }, { password: 0 })
-        const signedUsers = users.map(user => ({
-            name: user.name,
-            username: user.username,
-            email: user.email,
         }))
         res.status(201).json(signedUsers)
->>>>>>> e1c1fcaa008104b694ba891be0c4fafbaaa40785
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        res.status(404).json({ message: err.message })
     }
 });
 
-// router.get('/register', async (req, res) => {
-//     // const { query } = req.query
-//     try {
-//         const users = await User.find({ })
-//         res.status(204).json(users)
-//     } catch (err) {
-//         res.status(404).json({ message: err.message })
-//     }
-// })
 router.get('/register', async (req, res) => {
-<<<<<<< HEAD
-=======
-
->>>>>>> e1c1fcaa008104b694ba891be0c4fafbaaa40785
     try {
-        // Mengambil data user tanpa password
+        // RETRIEVE USER DATA WITHOUT PASSWORD
         const users = await User.find({}, { password: 0 })
 
-        // Mengembalikan data user dengan format yang diinginkan
+        // RETURNS USER DATA IN THE DESIRED FORMAT
         const registeredUsers = users.map(user => ({
             name: user.name,
             username: user.username,
@@ -65,16 +40,16 @@ router.get('/register', async (req, res) => {
     }
 })
 
-// Rute login dan register
-router.post('/login', loginUser);
-router.post('/register', registerUser);
+// POST LOGIN AND REGISTER ROUTE
+router.post('/login', loginUser)
+router.post('/register', registerUser)
 
 export default router;
 
 
-/* CARA MENGGUNAKAN ROUTER DI POSTMAN:
-1) nyalain server di terminal:   npm run start
-2) masuk ke postman, pilih method post, pilih endpoint user/register
-3) pastikan memilih body, pilih raw, pilih json, pilih tambahkan data yang ingin dikirimkan
-4) masukkan username dan password yang sesuai
+/* HOW TO USE A ROUTER IN POSTMAN:
+1) start the server in the terminal: npm run start
+2) enter postman, select method post, select endpoint user/register
+3) make sure to select body, select raw, select json, select add the data you want to send
+4) enter the appropriate username and password
 */

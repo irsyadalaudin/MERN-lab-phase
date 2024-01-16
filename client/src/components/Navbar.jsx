d86cc617eb7cbb86858e1b143fffe9a00f718c5c
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-01-16 12:16:17 +0200

    repaired style in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,13 +21,13 @@ const Navbar = ({ user, onLogout }) => {
     }
 
     return (
-        <nav className='bg-yellow-800 text-neutral-100 md:h-20 lg:h-14 py-3 lg:py-0 px-0 md:px-28 flex-col lg:flex-row flex justify-between items-center sticky top-0 hover:cursor-pointer'>
+        <nav className='bg-yellow-800 text-neutral-100 md:h-26 lg:h-14 pb-2 md:pb-5 lg:pb-0 px-0 xl:px-28 flex-col lg:flex-row flex justify-between items-center sticky top-0 hover:cursor-pointer'>
             <h1>
                 {/* <Link to='/' onClick={scrollToTopHome}>AYO MASAK</Link> */}
-                <Link to='/' onClick={scrollToTopHome}><img className='mt-0 xl:mt-2' src='https://i.postimg.cc/BZFTkfL1/ayo-masak-logo.png' alt='' /></Link>
 
+                <Link to='/' onClick={scrollToTopHome}><img className='justify-center' src='https://i.postimg.cc/BZFTkfL1/ayo-masak-logo.png' alt='' /></Link>
             </h1>
-            <div className='mt-5 md:mt-0 flex gap-4 no-underline text-sm sm:text-base'>
+            <div className='flex gap-4 no-underline text-sm sm:text-base'>
                 <Link to='/' onClick={scrollToTopHome} className='text-neutral-100 no-underline'>Home</Link>
                 <Link to='/' onClick={scrollToBottomRecipe} className='text-neutral-100 no-underline'>Recipe</Link>
                 <Link to='/contact-us' className='text-neutral-100 no-underline'>Contact us</Link>
@@ -35,7 +35,7 @@ const Navbar = ({ user, onLogout }) => {
                 <Link to='/account' className='text-neutral-100 no-underline'>Account</Link>
                 <Link to='/login' className='text-neutral-100 no-underline'>Login</Link>
                 {user && (
-                    <div className='flex justify-center bg-yellow-600 rounded-md gap-2 pl-1'>
+                    <div className='flex items-center bg-yellow-600 rounded-md gap-2 pl-1'>
                         <span className='text-black'>{user.username}</span>
                         <button onClick={onLogout} className='bg-yellow-600 text-black rounded-md hover:cursor-pointer hover:bg-yellow-700 border-none'>Logout</button>
                     </div>

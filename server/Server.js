a5aae62eb3887b6086c5f6ae10e4a6e966845be4
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-02-01 21:10:08 +0200

    installed dotenv in package.json,  repaired Server.js

--- server/Server.js
+++ server/Server.js
@@ -22,7 +22,7 @@ app.use('/user', userAuth)
 
 // SERVE THE REACT APP'S MAIN HTML FILE FOR ALL ROUTES
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
 
 // MONGODB DATABASE CONNECTION

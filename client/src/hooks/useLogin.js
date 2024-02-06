4d38efb2808bfaea012fa75397ceba78cd89b8b8
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-02-06 11:20:56 +0200

    added /user/Login   in useLogin.jsx

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -12,7 +12,7 @@ export const useLogin = () => {
 
         try {
             // const response = await fetch('http://localhost:4000/user/login', {
-            const response = await fetch('https://mern-backend-us5i.onrender.com', {
+            const response = await fetch('https://mern-backend-us5i.onrender.com/user/login', {
                 method: 'POST',
                 headers: { 'Content-type': 'application/json' },
                 body: JSON.stringify({ identifier, password })

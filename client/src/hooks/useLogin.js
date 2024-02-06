09653f0568eca90996203aba169e54288885ff95
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-02-06 11:18:09 +0200

    changed useLogin.jsx

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -11,7 +11,8 @@ export const useLogin = () => {
         setError(null)
 
         try {
-            const response = await fetch('http://localhost:4000/user/login', {
+            // const response = await fetch('http://localhost:4000/user/login', {
+            const response = await fetch('https://mern-backend-us5i.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-type': 'application/json' },
                 body: JSON.stringify({ identifier, password })

324fe8822f766f2662cfa12f90136e2852cc07b5
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-06-17 11:37:15 +0300

    removed unnecessary style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,13 +22,13 @@ const Navbar = ({ user, onLogout }) => {
     }
 
     return (
-        <nav className='bg-yellow-800 text-neutral-100  lg:h-14 pb-2 lg:pb-0 px-0 lg:px-18 xl:px-28 flex-col lg:flex-row flex justify-between items-center sticky top-0 hover:cursor-pointer'>
+        <nav className='bg-yellow-800 text-neutral-100 lg:h-14 pb-2 lg:pb-0 px-0 lg:px-18 xl:px-28 flex-col lg:flex-row flex justify-between items-center sticky top-0 hover:cursor-pointer'>
             <h1>
                 {/* <Link to='/' onClick={scrollToTopHome}>AYO MASAK</Link> */}
 
                 <Link to='/' onClick={scrollToTopHome}><img className='justify-center w-22 mt-0 md:mt-1 lg:mt-2 xl:mt-1 mx-0 lg:mx-10 xl:mx-0' src='https://i.postimg.cc/J4w6KWXw/ayo-masak-logo2.png' alt='ayo masak logo' /></Link>
             </h1>
-            <div className='flex gap-4 no-underline text-xs sm:text-base ml-0.5 sm:ml-0'>
+            <div className='flex gap-4 text-xs sm:text-base ml-0.5 sm:ml-0'>
                 <Link to='/' onClick={scrollToTopHome} className='text-neutral-100 no-underline'>Home</Link>
                 <Link to='/' onClick={scrollToBottomRecipe} className='text-neutral-100 no-underline'>Recipe</Link>
                 <Link to='/contact-us' className='text-neutral-100 no-underline'>Contact us</Link>

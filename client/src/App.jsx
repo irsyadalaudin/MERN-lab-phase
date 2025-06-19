446b784ff8d5ac45aa92b947497d44a72369bdf3
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-06-19 16:48:02 +0700

    chore: temporarily disable `login` route and `import` for future use in App.jsx: - comment out login `import` and `route` - comment out temporarily unused code for future reference

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Recipe from './components/Recipe'
 import ContactUs from './components/ContactUs'
 import AboutUs from './components/AboutUs'
 import Account from './components/Account'
-import Login from './pages/Login'
+// import Login from './pages/Login'
 import Register from './pages/Register'
 /*
 import { useEffect } from 'react'
@@ -51,7 +51,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/' element={<Recipe />} />
-                <Route path='/login' element={<Login />} />
+                {/* <Route path='/login' element={<Login />} /> */}
                 <Route path='/register' element={<Register />} />
                 <Route path='/contact-us' element={<ContactUs />} />
                 <Route path='/about-us' element={<AboutUs />} />
